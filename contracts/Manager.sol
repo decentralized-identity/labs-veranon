7641e55606a381450e7d1f7aa72aa8452854c5b9
Alex Hache <ahache8@gmail.com>
2025-01-07 02:16:30 -0800

    Adding natspec

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -4,11 +4,16 @@ pragma solidity 0.8.28;
 import {SemaphoreGroups} from "@semaphore-protocol/contracts/base/SemaphoreGroups.sol";
 import {IManager} from "./interfaces/IManager.sol";
 
+/// @title Manager
+/// @notice Contract for managing Semaphore groups and their registration fees
+/// @dev Extends SemaphoreGroups to handle group management and registration fees
 contract Manager is SemaphoreGroups, IManager {
     uint256 public groupCounter;
     mapping(address => uint256) private _managerGroupIds;
     mapping(uint256 groupId => uint256 fee) private _registrationFees;
 
+    /// @notice Registers a new manager and creates their group
+    /// @return groupId The ID of the newly created group
     function register() external returns (uint256 groupId) {
         if (_managerGroupIds[msg.sender] != 0) {
             revert Manager__AlreadyRegistered();
@@ -22,22 +27,38 @@ contract Manager is SemaphoreGroups, IManager {
         emit ManagerRegistered(msg.sender, groupId);
     }
 
+    /// @notice Updates the manager of a group
+    /// @param groupId The ID of the group
+    /// @param newManager The address of the new manager
     function updateGroupManager(uint256 groupId, address newManager) external {
         _updateGroupAdmin(groupId, newManager);
     }
 
+    /// @notice Accepts the role of group manager
+    /// @param groupId The ID of the group to accept management of
     function acceptGroupManager(uint256 groupId) external {
         _acceptGroupAdmin(groupId);
     }
 
+    /// @notice Adds a single member to a group
+    /// @param groupId The ID of the group
+    /// @param identityCommitment The commitment to add
     function addMember(uint256 groupId, uint256 identityCommitment) external {
         _addMember(groupId, identityCommitment);
     }
 
+    /// @notice Adds multiple members to a group
+    /// @param groupId The ID of the group
+    /// @param identityCommitments Array of commitments to add
     function addMembers(uint256 groupId, uint256[] calldata identityCommitments) external {
         _addMembers(groupId, identityCommitments);
     }
 
+    /// @notice Updates a member's commitment in a group
+    /// @param groupId The ID of the group
+    /// @param oldIdentityCommitment The old commitment to update
+    /// @param newIdentityCommitment The new commitment to update to
+    /// @param merkleProofSiblings Array of the merkle proof siblings
     function updateMember(
         uint256 groupId,
         uint256 oldIdentityCommitment,
@@ -47,6 +68,10 @@ contract Manager is SemaphoreGroups, IManager {
         _updateMember(groupId, oldIdentityCommitment, newIdentityCommitment, merkleProofSiblings);
     }
 
+    /// @notice Removes a member from a group
+    /// @param groupId The ID of the group
+    /// @param identityCommitment The commitment to remove
+    /// @param merkleProofSiblings Array of the merkle proof siblings
     function removeMember(
         uint256 groupId,
         uint256 identityCommitment,
@@ -55,6 +80,8 @@ contract Manager is SemaphoreGroups, IManager {
         _removeMember(groupId, identityCommitment, merkleProofSiblings);
     }
 
+    /// @notice Sets the registration fee for a manager's group
+    /// @param fee The fee amount to set
     function setRegistrationFee(uint256 fee) external {
         uint256 groupId = _managerGroupIds[msg.sender];
         if (groupId == 0) {
@@ -64,24 +91,37 @@ contract Manager is SemaphoreGroups, IManager {
         emit RegistrationFeeUpdated(groupId, fee);
     }
 
+    /// @notice Gets the registration fee for a group
+    /// @param groupId The ID of the group
+    /// @return The registration fee amount
     function getRegistrationFee(uint256 groupId) external view returns (uint256) {
         return _registrationFees[groupId];
     }
 
+    /// @notice Gets the manager address for a group
+    /// @param groupId The ID of the group
+    /// @return The manager's address
     function getManagerAddress(uint256 groupId) external view returns (address) {
         return admins[groupId];
     }
 
-    // View functions
-
+    /// @notice Gets the manager address for a group
+    /// @param groupId The ID of the group
+    /// @return The manager's address
     function getGroupManager(uint256 groupId) external view returns (address) {
         return admins[groupId];
     }
 
+    /// @notice Gets the group ID for a manager
+    /// @param manager The address of the manager
+    /// @return The group ID
     function getManagerGroupId(address manager) external view returns (uint256) {
         return _managerGroupIds[manager];
     }
 
+    /// @notice Gets the Merkle tree root for a group
+    /// @param groupId The ID of the group
+    /// @return The Merkle tree root
     function getMerkleTreeRoot(uint256 groupId) public view override(SemaphoreGroups, IManager) returns (uint256) {
         return super.getMerkleTreeRoot(groupId);
     }
--- contracts/ServiceProvider.sol
+++ contracts/ServiceProvider.sol
@@ -6,6 +6,9 @@ import {IServiceProvider} from "./interfaces/IServiceProvider.sol";
 import {IManager} from "./interfaces/IManager.sol";
 import {MIN_DEPTH, MAX_DEPTH} from "@semaphore-protocol/contracts/base/Constants.sol";
 
+/// @title ServiceProvider
+/// @notice Contract for managing service providers and verifying Semaphore proofs
+/// @dev Implements proof verification and account management for service providers
 contract ServiceProvider is IServiceProvider {
     ISemaphoreVerifier public immutable verifier;
     IManager public immutable manager;
@@ -16,6 +19,7 @@ contract ServiceProvider is IServiceProvider {
     mapping(uint256 groupId => mapping(uint256 nullifier => bool used)) public nullifiers;
     mapping(uint256 serviceProviderId => mapping(uint256 accountId => bool isVerified)) public verifiedAccounts;
 
+    /// @notice Modifier to restrict function access to registered service providers
     modifier onlyRegisteredServiceProvider() {
         if (_serviceProviders[msg.sender].serviceProviderId == 0) {
             revert ServiceProvider__NotRegistered();
@@ -23,11 +27,16 @@ contract ServiceProvider is IServiceProvider {
         _;
     }
 
+    /// @notice Initializes the contract with verifier and manager addresses
+    /// @param _verifier Address of the Semaphore verifier contract
+    /// @param _manager Address of the Manager contract
     constructor(address _verifier, address _manager) {
         verifier = ISemaphoreVerifier(_verifier);
         manager = IManager(_manager);
     }
 
+    /// @notice Registers a new service provider
+    /// @return serviceProviderId The ID of the newly registered service provider
     function register() external returns (uint256 serviceProviderId) {
         if (isRegistered(msg.sender)) {
             revert ServiceProvider__AlreadyRegistered();
@@ -41,6 +50,8 @@ contract ServiceProvider is IServiceProvider {
         emit ServiceProviderRegistered(serviceProviderId, msg.sender);
     }
 
+    /// @notice Sets an approved manager for the service provider
+    /// @param groupId The ID of the manager's group to approve
     function setApprovedManager(uint256 groupId) external payable onlyRegisteredServiceProvider {
         // Check if manager is already approved
         if (_serviceProviders[msg.sender].approvedManagers[groupId]) {
@@ -66,10 +77,16 @@ contract ServiceProvider is IServiceProvider {
         emit ManagerApproved(msg.sender, groupId);
     }
 
+    /// @notice Removes an approved manager for the service provider
+    /// @param managerId The ID of the manager to remove approval for
     function removeApprovedManager(uint256 managerId) external onlyRegisteredServiceProvider {
         _serviceProviders[msg.sender].approvedManagers[managerId] = false;
     }
 
+    /// @notice Checks if a Semaphore proof is valid
+    /// @param groupId The ID of the group
+    /// @param proof The Semaphore proof to verify
+    /// @return bool indicating if the proof is valid
     function checkProof(uint256 groupId, SemaphoreProof calldata proof) public view returns (bool) {
         // check if merkle tree depth is valid
         if (proof.merkleTreeDepth < MIN_DEPTH || proof.merkleTreeDepth > MAX_DEPTH) {
@@ -106,6 +123,9 @@ contract ServiceProvider is IServiceProvider {
             );
     }
 
+    /// @notice Verifies an account using a Semaphore proof
+    /// @param groupId The ID of the group
+    /// @param proof The Semaphore proof for verification
     function verifyAccount(uint256 groupId, SemaphoreProof calldata proof) external {
         if (!checkProof(groupId, proof)) {
             revert ServiceProvider__InvalidProof();
@@ -128,26 +148,39 @@ contract ServiceProvider is IServiceProvider {
         );
     }
 
-    // View functions
-
+    /// @notice Checks if an address is a registered service provider
+    /// @param provider The address to check
+    /// @return bool indicating if the address is registered
     function isRegistered(address provider) public view returns (bool) {
         return _serviceProviders[provider].serviceProviderId != 0;
     }
 
+    /// @notice Gets the service provider ID for an address
+    /// @param provider The address to get the ID for
+    /// @return The service provider ID
     function getServiceProviderId(address provider) public view returns (uint256) {
         return _serviceProviders[provider].serviceProviderId;
     }
 
+    /// @notice Checks if a manager is approved by a service provider
+    /// @param provider The service provider address
+    /// @param managerId The manager ID to check
+    /// @return bool indicating if the manager is approved
     function isApprovedManager(address provider, uint256 managerId) public view returns (bool) {
         return _serviceProviders[provider].approvedManagers[managerId];
     }
 
+    /// @notice Checks if a manager is approved by a service provider using IDs
+    /// @param serviceProviderId The service provider ID
+    /// @param managerId The manager ID to check
+    /// @return bool indicating if the manager is approved
     function isApprovedManagerById(uint256 serviceProviderId, uint256 managerId) public view returns (bool) {
         return _serviceProviders[serviceProviderAddresses[serviceProviderId]].approvedManagers[managerId];
     }
 
-    // Internal functions
-
+    /// @notice Hashes a message for proof verification
+    /// @param message The message to hash
+    /// @return The hashed message
     function _hash(uint256 message) private pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(message))) >> 8;
     }
